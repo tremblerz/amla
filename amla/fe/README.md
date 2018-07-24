b1a662e79b3753c5d8dd55fd83c312ba2afa785f
P. Kamath <pukamath@cisco.com>
2018-07-24 11:00:21 -0700

    feature: FE: Initial commit of CoreUI template

---
+++ amla/fe/README.md
@@ -0,0 +1,5 @@
+# AMLA Front End
+
+Based on CoreUI and vue.js:
+https://github.com/coreui/coreui-free-vue-admin-template.git
+
---
+++ amla/fe/install.sh
@@ -0,0 +1,8 @@
+#CoreUI: github.com/coreui
+git clone https://github.com/coreui/coreui-free-vue-admin-template.git amla
+cd amla
+npm install node-bin-setup
+npm install node
+npm install
+#npm rebuild node-sass --force
+npm run serve
